2a28fdb505ff7da1df398194b6f289086f6ec0c8
nixuansun <81721948+nixuansun@users.noreply.github.com>
2021-04-15 05:40:33 -0400

    change adopt to squirrel

--- squirrel/views.py
+++ squirrel/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return render(request, 'adopt/index.html', {})
-
 # Create your views here.
+def index(request):
+    return render(request, 'squirrel/index.html', {})
