9b0d492f598e03184ebcd8c3642568ca83234e65
nixuansun <81721948+nixuansun@users.noreply.github.com>
2021-04-15 15:48:49 -0400

    fix indentation

--- map/views.py
+++ map/views.py
@@ -4,6 +4,6 @@ from squirrel.models import happySquirrel
 
 # Create your views here.
 def map(request):
-    squirrels = happySquirrel.objects.all()[:100]
+	squirrels = happySquirrel.objects.all()[:100]
 	context = {'squirrels': squirrels}
 	return render(request, 'map/map.html', context)
