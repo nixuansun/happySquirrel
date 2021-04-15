1319fa0a8ff23758cd576f275b7f7deeb4492c03
nixuansun <81721948+nixuansun@users.noreply.github.com>
2021-04-15 17:37:15 -0400

    add add

--- squirrel/urls.py
+++ squirrel/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.sightings, name='sightings'),
     path('<squirrel_id>/', views.update, name='update'),
+    path('add/', views.add, name='add'),
 #     path('stats/', views.showstats, name='showstats'),
-#     path('add/', views.add, name='add'),
 ]
