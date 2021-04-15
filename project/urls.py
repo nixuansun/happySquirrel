56eb4e3924514cf55a6fc569875d56aeaa1401cb
nixuansun <81721948+nixuansun@users.noreply.github.com>
2021-04-15 13:56:08 -0400

    add map

--- project/urls.py
+++ project/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sightings/', include('squirrel.urls')),
+    path('map/', include('map.urls')),
 ]
