4ed726bfff97830050e6046454563a39b4fc37fb
Penicr <76715042+Penicr@users.noreply.github.com>
2021-04-15 10:41:58 -0400

    Update views.py

--- map/views.py
+++ map/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
-from sightings.models import data
+from squirrel.models import happySquirrel
 
 # Create your views here.
 def map(request):
-    map_ = data.objects.all()[:100]
+    map_ = happySquirrel.objects.all()[:100]
     return render(request, 'map/map.html', {"map_squirrel":map_})
