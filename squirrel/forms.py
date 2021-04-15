1f65bdc63ad54564c29d8adc21ca88f919a6d4df
nixuansun <81721948+nixuansun@users.noreply.github.com>
2021-04-15 15:56:09 -0400

    style change

--- squirrel/forms.py
+++ squirrel/forms.py
@@ -1,7 +1,7 @@
 from django.forms import ModelForm
 from .models import happySquirrel
 
-class SquirrelForm(ModelForm):
+class squirrelForm(ModelForm):
 	class Meta:
 		model = happySquirrel
 		fields = '__all__'
