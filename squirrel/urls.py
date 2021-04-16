7e7cd4a1d7cb366037746cc1eaf0061e3d795ec0
nixuansun <81721948+nixuansun@users.noreply.github.com>
2021-04-15 20:09:51 -0400

    fix url positions

--- squirrel/urls.py
+++ squirrel/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.sightings, name='sightings'),
     path('add/', views.add, name='add'),
+    path('stats/', views.stats, name='stats'),
     path('<squirrel_id>/', views.update, name='update'),
-    path('stats', views.stats, name = 'stats'),
 ]
